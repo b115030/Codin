5f651e3056eb6496b0cb7739ee56f041dccef23c
b115030 <iitiangokul@gmail.com>
2020-08-14 22:24:43 +0530

    ADDED: Comments and Indentation to the function shell scripts

--- loopShellScripts/functions/PrimePalindrome.sh
+++ loopShellScripts/functions/PrimePalindrome.sh
@@ -2,20 +2,20 @@
 
 IsPrime () {
 flag=0;
-sqrtn=$(bc <<< "scale=0; sqrt(($n))")
-for (( i=2;i<=sqrtn;i++ ))
+sqrtn=$(bc <<< "scale=0; sqrt(($n))") # Calculating the Square root of the number.
+for (( i=2;i<=sqrtn;i++ )) # It has been proved that if there are no factors until square root of a number the number is prime.
 do
-if [ $((n % i)) -eq 0 ]
-then
-flag=1
-fi
+	if [ $((n % i)) -eq 0 ] # Checking if there are any factors of the number.
+	then
+		flag=1
+	fi
 done
 if [ $flag -eq 1 ]
 then
-return 1
+	return 1 # Not Prime.
 elif [ $flag -eq 0 ]
 then
-return 0
+	return 0 # Prime
 fi
 }
 PalindromeGenerator (){
@@ -23,9 +23,9 @@ number=$n
 reverse=0
 while [ $number -gt 0 ]
 do
-a=`expr $number % 10 `
-number=`expr $number / 10 `
-reverse=`expr $reverse \* 10 + $a`
+	a=`expr $number % 10 ` # Ones place is stored in a
+	number=`expr $number / 10 `  # The ones place digit is completely removed from the number
+	reverse=`expr $reverse \* 10 + $a`  # The digit stored in a is the LSB now
 done
 return $reverse
 }
@@ -34,18 +34,18 @@ return $reverse
 while [ 1 ]
 do
 	read -p "Enter a Prime Number" n
-	IsPrime $n
+	IsPrime $n #Checking if Number is Prime
 	ret=$?
 	if [ $ret -eq 0 ]
 	then 
-		break
+		break # If Prime then execution moves forward
 	fi 
 done
-PalindromeGenerator $n
+PalindromeGenerator $n # Palindrome of the number is created
 Reverse=$?
-IsPrime $reverse
+IsPrime $reverse # the Palindrome is Checked for Prime or not
 ret=$?
-if [ $ret -eq 0 ]
+if [ $ret -eq 0 ] # If the number is Prime
 then
 	echo "Palindrome of " $n "which is " $reverse "is also Prime"
 fi
--- loopShellScripts/functions/palindrome2numbers.sh
+++ loopShellScripts/functions/palindrome2numbers.sh
@@ -1,25 +1,26 @@
 #!/bin/bash -x
 PalindromeGenerator (){
-number=$1
+number=$1 #The first number
 reverse1=0
 while [ $number -gt 0 ]
 do
-a=`expr $number % 10 `
-number=`expr $number / 10 `
-reverse1=`expr $reverse1 \* 10 + $a`
+	a=`expr $number % 10 ` # Ones place is stored in a
+	number=`expr $number / 10 ` # The ones place digit is completely removed from the number
+	reverse1=`expr $reverse1 \* 10 + $a` # The digit stored in a is the LSB now
 done
 
-number=$2
+number=$2 # The second number
 reverse2=0
 while [ $number -gt 0 ]
 do
-a=`expr $number % 10 `
-number=`expr $number / 10 `
-reverse2=`expr $reverse2 \* 10 + $a`
+	a=`expr $number % 10 ` # Ones place is stored in a
+	number=`expr $number / 10 ` # The ones place digit is completely removed from the number   
+	reverse2=`expr $reverse2 \* 10 + $a` # The digit stored in a is the LSB now 
 done
-return $reverse1 $reverse2
+return $reverse1 $reverse2 # Return bothe the reversed numbers
 }
 read -p "enter the numbers" n
 read p
-PalindromeGenerator $n $p
+PalindromeGenerator $n $p # Functions Called
 echo $reverse1 $reverse2
+
