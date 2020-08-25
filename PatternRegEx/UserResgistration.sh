e69930001affe4e5a06a323cb7e4e2d10798f5d6
b115030 <iitiangokul@gmail.com>
2020-08-25 22:06:12 +0530

    MODIFIED: regex for phone number checking added

--- PatternRegEx/UserResgistration.sh
+++ PatternRegEx/UserResgistration.sh
@@ -1,16 +1,22 @@
 #!/bin/bash -x
 read -p "Enter a first name" firstName
 read -p "Enter a valid Email" email
-
+read -p "Enter a valid phone number" phone
+validPhone="^(91) [0-9]*{10}"
 validName="^[A-Z][a-z]{2,} [A-Z][a-z]{2,}$"
 validEmailId="^[a-zA-Z]{3}\.?[a-zA-Z0-9\$\_\+]*\@[a-z0-9]*\.(co|in|com|net)"
 if [[ $firstName =~ $validFirstName ]]
 then
 	if [[ $email =~ $validEmailId ]]
 	then
-		echo Valid
+		if [[ $phone =~ $validPhone ]]
+		then
+			echo Valid
+		else 
+			echo No
+		fi
 	else
-		echo No
+		echo Invalid
 	fi
 else 
 	echo No
