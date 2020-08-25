72937986124c74cf14c49d53b98b26b083380394
b115030 <iitiangokul@gmail.com>
2020-08-25 21:42:30 +0530

    MODIFIED: regex added to check email

--- PatternRegEx/UserResgistration.sh
+++ PatternRegEx/UserResgistration.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
+#!/bin/bash -x
 read -p "Enter a first name" firstName
+read -p "Enter a valid Email" email
 
-validFirstName="^[A-Z][a-z]{2,} [A-Z][a-z]{2,}$"
+validName="^[A-Z][a-z]{2,} [A-Z][a-z]{2,}$"
+validEmailId="^[a-zA-Z]{3}\.?[a-zA-Z0-9\$\_\+]*\@[a-z0-9]*\.(co|in|com|net)"
 if [[ $firstName =~ $validFirstName ]]
 then
+	if [[ $email =~ $validEmailId ]]
+	then
 		echo Valid
 	else
-		echo Not Valid
+		echo No
+	fi
+else 
+	echo No
 fi
 
