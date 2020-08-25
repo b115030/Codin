1345e99e368a5c60e28268aab4d0038f7e864e4b
b115030 <iitiangokul@gmail.com>
2020-08-25 23:45:25 +0530

    MODIFIED: regex for checking valid password added

--- PatternRegEx/UserResgistration.sh
+++ PatternRegEx/UserResgistration.sh
@@ -1,11 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
 read -p "Enter a first name" firstName
-
-validFirstName="^[A-Z][a-z]{2,} [A-Z][a-z]{2,}$"
-if [[ $firstName =~ $validFirstName ]]
+read -p "Enter a valid Email" email
+read -p "Enter a valid phone number" phone
+read -p "Enter a valid Password" password
+validPassword="^[A-Za-z0-9\#\$\+\*\@]{8,}$" 
+validPhone="^(91) [0-9]*{10}"
+validName="^[A-Z][a-z]{2,} [A-Z][a-z]{2,}$"
+validEmailId="^[a-zA-Z]{3}\.?[a-zA-Z0-9\$\_\+]*\@[a-z0-9]*\.(co|in|com|net)"
+if [[ $firstName =~ $validName ]]
 then
-		echo Valid
+	if [[ $email =~ $validEmailId ]]
+	then
+		if [[ $phone =~ $validPhone ]]
+		then
+			if [[ $password =~ $validPassword ]]
+			then
+				echo Valid
+			else
+				echo Invalid Password
+			fi
+		else 
+			echo Invalid Phone Number
+		fi
 	else
-		echo Not Valid
+		echo Invalid Email ID
+	fi
+else 
+	echo Invalid Name
 fi
 
